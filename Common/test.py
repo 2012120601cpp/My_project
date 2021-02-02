c0c5fbd8c99767d319ece3fd611acb349769b172
2012120601cpp <379609962@qq.com>
2021-02-02 14:08:31 +0800

    new

---
+++ Common/test.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+class MyPgsql(object):
+    def __init__(self):
+        pass
+
+    def run(self):
+        while True:
+            pass
+
+
+
+def main(self):
+    mypgsql =MyPgsql()
+    mypgsql.run()
+
+
+
+if __name__ == '__main__':
+    main()
