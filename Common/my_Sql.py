78223bd1f69f4b819507c92833d1556fca49a447
2012120601cpp <379609962@qq.com>
2021-02-23 14:41:57 +0800

    更新2021/2/23  10:49

--- Common/my_Sql.py
+++ Common/my_Sql.py
@@ -1,14 +1,14 @@
-# 需求总数
-sum_requirement = 'select count(*) from  where  ;'
+# 版本下需求总数
+sum_requirement = 'select count(*) from     where  ;'
 # 开发提测需求数
-dev_complete_requirement = 'select count(*) from  where  ;'
+dev_complete_requirement = 'select count(*) from     where  ;'
 # 测试完成需求数
-test_complete_requirement = 'select count(*) from  where  ;'
+test_complete_requirement = 'select count(*) from     where  ;'
 # bug总数
-sum_bug = 'select count(*) from  where  ;'
+sum_bug = 'select count(*) from      where  ;'
 # 优先级为高及以上bug数
-serious_bug = 'select count(*) from  where  ;'
+serious_bug = 'select count(*) from      where  ;'
 # 需求详情地址
-requirement_addr = 'select count(*) from  where  ;'
+requirement_addr = 'select count(*) from      where  ;'
 # bug详情地址
-bug_addr = 'select count(*) from  where  ;'
+bug_addr = 'select count(*) from      where  ;'
